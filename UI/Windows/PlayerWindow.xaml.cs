dc731f5b36a46f2dfe72474793f877d7f000fb6d
Russell Newman <info@peartree.london>
2022-05-28 14:55:30 +0100

    added windows project files

---
+++ UI/Windows/PlayerWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using OpenTK.Wpf;
+
+namespace Octopus.Player.UI.Windows
+{
+    /// <summary>
+    /// Interaction logic for PlayerWindow.xaml
+    /// </summary>
+    public partial class PlayerWindow : Window
+    {
+        public PlayerWindow()
+        {
+            InitializeComponent();
+
+            var mainSettings = new GLWpfControlSettings 
+            { 
+                MajorVersion = 3, 
+                MinorVersion = 2,
+                RenderContinuously = false 
+            };
+            OpenTkControl.Start(mainSettings);
+        }
+
+        private void OpenTkControl_OnRender(TimeSpan delta)
+        {
+            GL.ClearColor(Color4.Blue);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+        }
+    }
+}
