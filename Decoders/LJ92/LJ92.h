4b0526d09d156c92c0b42efbd1f15d442f63152b
Russ <info@peartree.london>
2022-06-12 23:56:56 +0100

    fixed macOS dll loading issue

--- Decoders/LJ92/LJ92.h
+++ Decoders/LJ92/LJ92.h
@@ -1,3 +1,13 @@
 #pragma once
 
+#ifdef __GNUC__
+
+#pragma GCC visibility push(default)
 extern "C" int TestMethod(int param);
+#pragma GCC visibility pop
+
+#else
+
+extern "C" int TestMethod(int param);
+
+#endif
