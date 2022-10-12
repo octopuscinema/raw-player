c284e5edd5708a1e196ccc43c9860a6203dae671
Russ <>
2022-10-12 18:31:07 +0100

    updated apply verison script to fix installer name

--- ApplyVersion.ps1
+++ ApplyVersion.ps1
@@ -6,6 +6,8 @@ try {
 	$doc.Save((Resolve-Path ".\UI\Windows\Player.UI.Windows.csproj"))
 	$installerVersionText = '"ProductVersion" = "8:' + $version + '"'
 	(Get-Content ".\Installer\Installer.vdproj") -replace '"ProductVersion" = "8:.+"', "$installerVersionText" | Set-Content ".\Installer\Installer.vdproj"
+	$installerFilename = '8:OCTOPUS-RAW-Player-' + $version + '-setup.msi'
+	(Get-Content ".\Installer\Installer.vdproj") -replace '8:OCTOPUS-RAW-Player-setup.msi', "$installerFilename" | Set-Content ".\Installer\Installer.vdproj"
 }
 catch {
 	exit 1
