2467133efe9f191e6556456ffe9f90a9ff129e7e
Russ <>
2024-06-03 00:52:19 +0100

    lut gpu formats

--- Core/IO/LUT/LUT3D.cs
+++ Core/IO/LUT/LUT3D.cs
@@ -16,30 +16,47 @@ namespace Octopus.Player.Core.IO.LUT
         public GPU.Compute.IImage ComputeImage { get; private set; }
         public GPU.Format Format { get { return ComputeImage.Format; } }
 
-        public LUT3D(GPU.Compute.IContext computeContext, string filePath, GPU.Format format = GPU.Format.RGBA8)
+        public LUT3D(GPU.Compute.IContext computeContext, string filePath, Format format = Format.RGBA8)
         {
             var lines = File.ReadLines(filePath);
-            Upload(computeContext, Parse(lines).ToArray(), format);
+            switch (format)
+            {
+                case GPU.Format.RGBA8:
+                    Upload(computeContext, Parse<byte>(lines).ToArray(), format);
+                    break;
+                case GPU.Format.RGBA16:
+                    Upload(computeContext, Parse<ushort>(lines).ToArray(), format);
+                    break;
+                default:
+                    throw new Exception("Unsupported LUT format");
+            }
         }
 
         public LUT3D(GPU.Compute.IContext computeContext, Assembly assembly, string resourceName, GPU.Format format = GPU.Format.RGBA8)
         {
-            var sourceStream = assembly.GetManifestResourceStream(resourceName);
-            var reader = new StreamReader(sourceStream);
+            //var sourceStream = assembly.GetManifestResourceStream(resourceName);
+            //var reader = new StreamReader(sourceStream);
 
-            IList<byte> data = new List<byte>();
-
-            var line = reader.ReadLine();
-            while (line != null)
+            using (var sourceStream = assembly.GetManifestResourceStream(resourceName))
+            using (var reader = new StreamReader(sourceStream))
             {
-                Parse(line, data);
-                line = reader.ReadLine();
+
+                switch (format)
+                {
+                    case GPU.Format.RGBA8:
+                        var data = new List<byte>();
+                        var line = reader.ReadLine();
+                        while (line != null)
+                        {
+                            Parse(line, data);
+                            line = reader.ReadLine();
+                        }
+                        Upload(computeContext, data.ToArray(), format);
+                        break;
+                    default:
+                        throw new Exception("Unsupported LUT format");
+                }
             }
-
-            reader.Dispose();
-            sourceStream.Dispose();
-
-            Upload(computeContext, data.ToArray(), format);
         }
 
         public void Dispose()
@@ -48,7 +65,7 @@ namespace Octopus.Player.Core.IO.LUT
                 ComputeImage.Dispose();
         }
 
-        void Parse(string line, IList<byte> data)
+        void Parse<T>(string line, IList<T> data)
         {
             // Clean the input
             var clean = line.Trim();
@@ -90,18 +107,30 @@ namespace Octopus.Player.Core.IO.LUT
                             float.TryParse(tokens[1], out rgb[1]) &&
                             float.TryParse(tokens[2], out rgb[2]))
                         {
-                            foreach (var channel in rgb)
-                                data.Add((byte)Math.Clamp(channel * 255.0f, 0.0f, 255.0f));
-                            data.Add(0);
+                            switch(data)
+                            {
+                                case IList<byte> dataByte:
+                                    foreach (var channel in rgb)
+                                        dataByte.Add((byte)Math.Clamp(channel * 255.0f, 0.0f, 255.0f));
+                                    dataByte.Add(0);
+                                    break;
+                                case IList<ushort> dataUshort:
+                                    foreach (var channel in rgb)
+                                        dataUshort.Add((ushort)Math.Clamp(channel * 65535.0f, 0.0f, 65535.0f));
+                                    dataUshort.Add(0);
+                                    break;
+                                default:
+                                    throw new Exception("Unsupported LUT format");
+                            }
                         }
                     }
                     break;
             }
         }
 
-        IList<byte> Parse(IEnumerable<string> lines)
+        IList<T> Parse<T>(IEnumerable<string> lines)
         {
-            IList<byte> data = new List<byte>();
+            IList<T> data = new List<T>();
 
             foreach (var line in lines)
                 Parse(line, data);
@@ -109,18 +138,37 @@ namespace Octopus.Player.Core.IO.LUT
             return data;
         }
 
-        void Upload(GPU.Compute.IContext computeContext, byte[] data, GPU.Format format)
+        void Upload<T>(GPU.Compute.IContext computeContext, T[] data, GPU.Format format)
         {
-            // Check input size
+            // Get input size
             var expectedSizeBytes = format.SizeBytes() * Dimensions.X * Dimensions.Y *Dimensions.Z;
-            if (expectedSizeBytes != data.Length)
-                throw new Exception("Unexpected LUT size");
             
             if (ComputeImage != null)
                 ComputeImage.Dispose();
 
-            ComputeImage = computeContext.CreateImage(Dimensions, format, GPU.Compute.MemoryDeviceAccess.ReadOnly,
-                GPU.Compute.MemoryHostAccess.NoAccess, data, GPU.Compute.MemoryLocation.Default, "lut");
+            switch (data)
+            {
+                case byte[] dataByte:
+                    if (expectedSizeBytes != data.Length * sizeof(byte))
+                        throw new Exception("Unexpected LUT size");
+                    ComputeImage = computeContext.CreateImage(Dimensions, format, GPU.Compute.MemoryDeviceAccess.ReadOnly,
+                        GPU.Compute.MemoryHostAccess.NoAccess, dataByte, GPU.Compute.MemoryLocation.Default, "lut");
+                    break;
+
+                case ushort[] dataUshort:
+                    if (expectedSizeBytes != data.Length * sizeof(ushort))
+                        throw new Exception("Unexpected LUT size");
+
+                    byte[] dataRaw = new byte[dataUshort.Length * sizeof(ushort)];
+                    Buffer.BlockCopy(dataUshort, 0, dataRaw, 0, dataRaw.Length);
+
+                    ComputeImage = computeContext.CreateImage(Dimensions, format, GPU.Compute.MemoryDeviceAccess.ReadOnly,
+                        GPU.Compute.MemoryHostAccess.NoAccess, dataRaw, GPU.Compute.MemoryLocation.Default, "lut");
+                    break;
+
+                default:
+                    throw new Exception("Unsupported LUT format");
+            }
         }
     }
 }
